7ed9e8074efa9a619463b510592277fe1f8acc5b
Eduardo Henrique <eduardo.hor@outlook.com>
2023-06-16 10:29:18 -0300

    adding tags search card

---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Busca de marcas</div>
+
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mb-3">
+                                <input-container-component titulo="ID" id="inputId" id-help="idHelp"
+                                    texto-opcional="Opcional. Informe o ID do registro">
+                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
+                                        placeholder="ID">
+                                </input-container-component>
+                            </div>
+                            <div class="col mb-3">
+                                <input-container-component titulo="Nome" id="inputNome" id-help="nomeHelp"
+                                    texto-opcional="Opcional. Informe o nome da marca">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp"
+                                        placeholder="Nome da marca">
+                                </input-container-component>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer d-flex justify-content-end">
+                        <button type="submit" class="btn btn-primary btn-sm">Pesquisar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
