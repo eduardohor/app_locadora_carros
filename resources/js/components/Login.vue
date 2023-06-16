a58adc52754dd2929d2a2ad73d774d4e78d3e790
Eduardo Henrique <eduardo.hor@outlook.com>
2023-06-16 09:26:26 -0300

    converting the home view to a vue component

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -8,7 +8,7 @@
                     <div class="card-body">
                         <form method="POST" action="" @submit.prevent="login($event)">
                             <input type="hidden" name="_token" :value="csrf_token">
-                            <div class="form-group row">
+                            <div class="form-group row mb-2">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
 
                                 <div class="col-md-6">
@@ -16,7 +16,7 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
+                            <div class="form-group row mb-2">
                                 <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
 
                                 <div class="col-md-6">
@@ -24,7 +24,7 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
+                            <div class="form-group row mb-2">
                                 <div class="col-md-6 offset-md-4">
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember">
